3fb3ce70f6945fe6fa2ad37ee325517334f4548f
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-02-14 21:21:47 +0700

    ⭐create top up confirmation comp

---
+++ src/components/topup/TopUpConfirmation.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+interface ITopUpNominalProps {
+  nominal: number;
+}
+const TopUpConfirmation: React.FC<ITopUpNominalProps> = (props) => {
+  return (
+    <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center items-center p-4 rounded-md bg-white">
+      <img src="assets/logo.png" alt="" className="w-10 h-10" />
+      <div className="mt-5">Anda yakin untuk Top Up sebesar</div>
+      <div className="font-bold text-2xl">
+        Rp. {props.nominal.toLocaleString("id-ID")}
+      </div>
+
+      <div className="flex flex-col gap-4 mt-4">
+        <button className="text-red-600">Ya, lanjutkan Top Up</button>
+        <button className="text-slate-400">Batalkan</button>
+      </div>
+    </div>
+  );
+};
+
+export default TopUpConfirmation;
