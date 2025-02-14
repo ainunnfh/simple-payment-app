82320c2b9bc2c811b5e438a9eb8780e140ab88ff
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-02-14 09:15:10 +0700

    ➕create routing for registration page

---
+++ src/app/registration-page/page.tsx
@@ -0,0 +1,12 @@
+import RegistrationForm from "@/components/registration/RegistrationForm";
+import React from "react";
+
+const RegistrationPage = () => {
+  return (
+    <div>
+      <RegistrationForm />
+    </div>
+  );
+};
+
+export default RegistrationPage;
