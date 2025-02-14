e097d571d225ef6d9d5a2a75ebee513b754db6f1
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-02-14 11:20:19 +0700

    💄homepage: design for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,21 @@
-import { Button } from "@/components/ui/button";
-// import Image from "next/image";
+import Feature from "@/components/homepage/Feature";
+import Promo from "@/components/homepage/Promo";
+import NavBar from "@/components/NavBar";
+import ProfileCard from "@/components/ProfileCard";
+import Saldo from "@/components/Saldo";
 
+// import Image from "next/image";
 export default function Home() {
-  return <Button>Click me</Button>;
+  return (
+    <NavBar>
+      <div className="mx-auto flex flex-col max-w-[1300px]">
+        <div className="flex justify-between p-4">
+          <ProfileCard name="Kristanto Wibowo" />
+          <Saldo />
+        </div>
+        <Feature />
+        <Promo />
+      </div>
+    </NavBar>
+  );
 }
