b53e3d4f38b6076d110bdc4faadf758366d6934e
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-02-14 12:14:03 +0700

    🆙navbar: add routing each pages

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,6 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 interface INavBarProps {
@@ -5,17 +8,31 @@ interface INavBarProps {
   children?: React.ReactNode;
 }
 const NavBar: React.FC<INavBarProps> = (props) => {
+  const pathname = usePathname();
+
+  const linkClasses = (path: string) =>
+    `hover:cursor-pointer ${
+      pathname === path ? "text-red-600 font-bold" : "hover:text-red-600"
+    }`;
   return (
     <>
-      <div className="flex justify-between items-center p-4 shadow shadow-gray-200 ">
-        <div className="flex gap-2 items-center">
-          <img className="w-5 h-5" src="assets/Logo.png" />
-          <span className="font-bold">SIMS PPOB</span>
-        </div>
+      <div className="flex justify-between items-center p-4 mx-auto shadow shadow-gray-200 ">
+        <Link href="/">
+          <div className="flex gap-2 items-center hover:text-red-600">
+            <img className="w-5 h-5" src="assets/Logo.png" />
+            <span className="font-bold">SIMS PPOB</span>
+          </div>
+        </Link>
         <div className="flex gap-4">
-          <div>Top Up</div>
-          <div>Transaction</div>
-          <div>Akun</div>
+          <Link href="/top-up-page">
+            <div className={linkClasses("/top-up-page")}>Top Up</div>
+          </Link>
+          <Link href="/transaction-page">
+            <div className={linkClasses("/transaction-page")}>Transaction</div>
+          </Link>
+          <Link href="/account-page">
+            <div className={linkClasses("/account-page")}>Akun</div>
+          </Link>
         </div>
       </div>
       <div className="">{props.children}</div>
