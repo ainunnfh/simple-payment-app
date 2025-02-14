b764d59146b27cfd057fc4a006b063776820c976
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-02-14 10:44:24 +0700

    ⭐create feature component

---
+++ src/components/homepage/Feature.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Feature = () => {
+  return (
+    <div className="flex gap-2 p-6">
+      <img src="assets/PBB.png" alt="PBB" />
+      <img src="assets/Listrik.png" alt="Listrik" />
+      <img src="assets/Pulsa.png" alt="Listrik" />
+      <img src="assets/PDAM.png" alt="Listrik" />
+      <img src="assets/PGN.png" alt="Listrik" />
+      <img src="assets/Televisi.png" alt="Listrik" />
+      <img src="assets/Musik.png" alt="Listrik" />
+      <img src="assets/Game.png" alt="Listrik" />
+      <img src="assets/Kurban.png" alt="Listrik" />
+      <img src="assets/Zakat.png" alt="Listrik" />
+      <img src="assets/paket-data.png" alt="Listrik" />
+    </div>
+  );
+};
+
+export default Feature;
