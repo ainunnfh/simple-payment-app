4813326527047fa80ed5a438a8e2658567871f01
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-02-15 14:49:27 +0700

    🆙usage account form comp

---
+++ src/app/account-page/page.tsx
@@ -0,0 +1,15 @@
+import AccountForm from "@/components/account/AccountForm";
+import NavBar from "@/components/NavBar";
+import React from "react";
+
+const AccountPage = () => {
+  return (
+    <NavBar>
+      <div className="mx-auto flex flex-col max-w-[1300px]">
+        <AccountForm></AccountForm>
+      </div>
+    </NavBar>
+  );
+};
+
+export default AccountPage;
