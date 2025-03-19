a5c1c3f45da055bac2e71a3c4d5c9df85f2d1b0f
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-03-19 22:54:32 +0700

    🆙transaction page: useTransaction context

--- src/app/transaction-page/page.tsx
+++ src/app/transaction-page/page.tsx
@@ -3,58 +3,37 @@ import NavBar from "@/components/NavBar";
 import ProfileCard from "@/components/ProfileCard";
 import Saldo from "@/components/Saldo";
 import TransactionHistory from "@/components/transaction/TransactionHistory";
+import { useTransaction } from "@/context/TransactionContext";
 import React from "react";
 
 const TransactionPage = () => {
+  const { transactions } = useTransaction();
+
   return (
     <NavBar>
       <div className="mx-auto flex flex-col max-w-[1300px]">
         <div className="flex justify-between p-4">
-          <ProfileCard name="Kristanto Wibowo" />
+          <ProfileCard name="John Doe" />
           <Saldo />
         </div>
         <div className="flex flex-col gap-2 p-4">
           <h1 className="font-bold">Semua Transaksi</h1>
-          <TransactionHistory
-            color="green-500"
-            amount={100000}
-            type={"+"}
-            date={"17 Agustus 2024"}
-            time={"13:10 WIB"}
-            desc={"Top Up Saldo"}
-          />
-          <TransactionHistory
-            color="red-600"
-            amount={400000}
-            type={"-"}
-            date={"17 Agustus 2024"}
-            time={"12:10 WIB"}
-            desc={"Pulsa Prabayar"}
-          />
-          <TransactionHistory
-            color="red-600"
-            amount={100000}
-            type={"-"}
-            date={"17 Agustus 2024"}
-            time={"11:10 WIB"}
-            desc={"Listrik Pascabayar"}
-          />
-          <TransactionHistory
-            color="green-500"
-            amount={500000}
-            type={"+"}
-            date={"17 Agustus 2024"}
-            time={"10:10 WIB"}
-            desc={"Top Up Saldo"}
-          />
-          <TransactionHistory
-            color="green-500"
-            amount={500000}
-            type={"+"}
-            date={"17 Agustus 2024"}
-            time={"10:10 WIB"}
-            desc={"Top Up Saldo"}
-          />
+
+          {transactions.length === 0 ? (
+            <p className="text-gray-500">Belum ada transaksi</p>
+          ) : (
+            transactions.map((trx) => (
+              <TransactionHistory
+                key={trx.id}
+                color={trx.color}
+                amount={trx.amount}
+                type={trx.type}
+                date={trx.date}
+                time={trx.time}
+                desc={trx.desc}
+              />
+            ))
+          )}
         </div>
         <button className="text-red-600">Show more</button>
       </div>
