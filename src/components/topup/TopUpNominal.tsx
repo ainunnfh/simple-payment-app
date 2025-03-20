a1199aff6e601c21efd0b65e8f1e79ae607d1e5f
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-03-20 20:28:51 +0700

    ➕top up nominal: add onclick props

--- src/components/topup/TopUpNominal.tsx
+++ src/components/topup/TopUpNominal.tsx
@@ -2,10 +2,14 @@ import React from "react";
 
 interface ITopUpNominalProps {
   nominal: number;
+  onClick?: (nominal: number) => void;
 }
 const TopUpNominal: React.FC<ITopUpNominalProps> = (props) => {
   return (
-    <button className="border-gray-200 border-2 rounded-md py-2 px-4 flex items-center justify-center w-[30%] min-w-[100px]">
+    <button
+      className="border-gray-200 border-2 rounded-md py-2 px-4 flex items-center justify-center w-[30%] min-w-[100px] "
+      onClick={() => props.onClick?.(props.nominal)}
+    >
       Rp. {props.nominal.toLocaleString("id-ID")}
     </button>
   );
